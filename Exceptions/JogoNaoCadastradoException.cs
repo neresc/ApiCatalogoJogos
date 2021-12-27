94d057102de6a85a3e374885cce2413349d6dfae
neresc <neresc@yahoo.com.br>
2021-12-27 16:41:49 -0300

    Add files via upload
    Projeto Catalogo de Jogos funcionando e rodando.

---
+++ Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCadastradoException: Exception    {
+        public JogoNaoCadastradoException()
+    : base("Este Jogo não está cadastrado")
+        { }
+    }
+}
